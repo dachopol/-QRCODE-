1588f5d0d1c1a0d804708e0b7f2295a785202a43
AnakinYoo <215334638+dachopol@users.noreply.github.com>
2026-09-22 23:11:40 +0700

    build: bump QuickQR Business to v13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.qrgenerator.kmpzqr"
     minSdk = 24
     targetSdk = 36
-    versionCode = 12
-    versionName = "12.0"
+    versionCode = 13
+    versionName = "13.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
