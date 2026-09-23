2cc1dbfa4aef3b19234f1b61477fee6e7f8d69ef
AnakinYoo <215334638+dachopol@users.noreply.github.com>
2026-09-23 09:21:33 +0700

    release: bump app to v15.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
     applicationId = "com.aistudio.qrgenerator.kmpzqr"
     minSdk = 24
     targetSdk = 36
-    versionCode = 14
-    versionName = "14.0"
+    versionCode = 15
+    versionName = "15.0"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
