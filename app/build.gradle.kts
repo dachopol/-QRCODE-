1d87af178626b5c47e597f7247359c0705caa26e
AnakinYoo <215334638+dachopol@users.noreply.github.com>
2026-09-26 05:20:37 +0700

    release: bump QuickQR Business to v18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
     applicationId = "com.aistudio.qrgenerator.kmpzqr"
     minSdk = 24
     targetSdk = 36
-    versionCode = 17
-    versionName = "17.0"
+    versionCode = 18
+    versionName = "18.0"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
